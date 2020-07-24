ac54ea0f6887b5dc23bd36866b0121b2060bf0f9
MrAjMann <anthonyjmann87@gmail.com>
2020-07-24 23:12:03 +1000

    Initialised the codewars.js file which will be responsible for requesting access to the codewars api and retrieving user data aswell as the kata

---
+++ codewars.js
@@ -0,0 +1,9 @@
+console.log(fetch('https://cors-anywhere.herokuapp.com/https://www.codewars.com/api/v1/users/mrAmann', {
+    method: 'get',
+    headers: {
+        'Content-Type': 'application/json',
+
+
+    }
+}));
+
