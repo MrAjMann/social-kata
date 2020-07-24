690c1845185dcefce9c951d4d2756ead10845a1f
MrAjMann <anthonyjmann87@gmail.com>
2020-07-24 23:14:32 +1000

    gets current tab information and prints the url in the background console

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // Access chrome tabs and listen for social accounts
 chrome.tabs.onActivated.addListener(tab => {
-    console.log(tab)
+    // console.log(tab)
     chrome.tabs.get(tab.tabId, current_tab_info => {
         console.log(current_tab_info.url)
     });
