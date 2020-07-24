737aa415c1e5ea3fe0b88c4d0e331a81e573d8a6
MrAjMann <anthonyjmann87@gmail.com>
2020-07-24 19:15:42 +1000

    Added script to get current URL from browser

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
-console.log('from background')
+// Access chrome tabs and listen for social accounts
+chrome.tabs.onActivated.addListener(tab => {
+    console.log(tab)
+    chrome.tabs.get(tab.tabId, current_tab_info => {
+        console.log(current_tab_info.url)
+    });
+});
